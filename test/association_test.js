ba38ccab0361b05cb189f403feec40ac30967828
Matt Logan <mlogan1313@gmail.com>
2016-11-22 12:55:02 -0600

    added full graph test

--- test/association_test.js
+++ test/association_test.js
@@ -28,4 +28,27 @@ describe('Associations', () => {
         done();
       });
   });
+
+  it('saves a full relation graph', (done) => {
+    User.findOne({ name: 'Joe' })
+      .populate({
+        path: 'blogPosts',
+        populate: {
+          path: 'comments',
+          model: 'comment',
+          populate: {
+            path: 'user',
+            model: 'user'
+          }
+        }
+      })
+      .then((user) => {
+        assert(user.name === 'Joe');
+        assert(user.blogPosts[0].title === 'JS is great');
+        assert(user.blogPosts[0].comments[0].content === 'Congrats on great post, lulz');
+        assert(user.blogPosts[0].comments[0].user.name === 'Joe');
+
+        done();
+      });
+  });
 });
