76036b1e73010880f5a3bbddb1ef28c437b40acf
Matt Logan <mlogan1313@gmail.com>
2016-11-22 10:54:41 -0600

    changed post count test to like test

--- test/update_test.js
+++ test/update_test.js
@@ -5,7 +5,7 @@ describe('Updating records', () => {
   let joe;
 
   beforeEach((done) => {
-    joe = new User({ name: 'Joe', postCount: 0 });
+    joe = new User({ name: 'Joe', likes: 0 });
     joe.save()
       .then(() => done());
   });
@@ -50,11 +50,11 @@ describe('Updating records', () => {
     )
   });
 
-  xit('a user can have their post count incremented by 1', (done) => {
-    User.update({ name: 'Joe' }, { $inc: { postCount: 1} })
+  it('a user can have their like count incremented by 1', (done) => {
+    User.update({ name: 'Joe' }, { $inc: { likes: 10 } })
       .then(() => User.findOne({ name: 'Joe' }))
       .then((user) => {
-        assert(user.postCount === 1);
+        assert(user.likes === 10);
         done();
       });
   });
