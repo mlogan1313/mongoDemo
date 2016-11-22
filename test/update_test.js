0fac6d942e7d70fb6e23d19977b767e1526ba9b6
Matt Logan <mlogan1313@gmail.com>
2016-11-22 10:00:18 -0600

    completed user testing

--- test/update_test.js
+++ test/update_test.js
@@ -5,7 +5,7 @@ describe('Updating records', () => {
   let joe;
 
   beforeEach((done) => {
-    joe = new User({ name: 'Joe' });
+    joe = new User({ name: 'Joe', postCount: 0 });
     joe.save()
       .then(() => done());
   });
@@ -48,5 +48,14 @@ describe('Updating records', () => {
       User.findByIdAndUpdate({ _id: joe._id }, { name: 'Alex' }),
       done
     )
+  });
+
+  it('a user can have their post count incremented by 1', (done) => {
+    User.update({ name: 'Joe' }, { $inc: { postCount: 1} })
+      .then(() => User.findOne({ name: 'Joe' }))
+      .then((user) => {
+        assert(user.postCount === 1);
+        done();
+      });
   });
 });
